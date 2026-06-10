8309f467fda5b94ee5259e9dbd0553509560fac2
Moureen100 <nassangamoureen100@gmail.com>
2026-06-10 15:07:20 +0300

    control structure

---
+++ ControlStructure.py
@@ -0,0 +1,10 @@
+       
+    #write a program that takes a number as input and checks if its positive,negative or zero
+number = int(input("Enter number"))
+if number >0:
+    print("number is a positive number")   
+elif number < 0:
+    print("number is negative")  
+else:
+    print("number is zero")    
+  
